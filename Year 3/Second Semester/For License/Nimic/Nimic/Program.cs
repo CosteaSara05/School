625974ad1ff23895448db0d792bba5c54fb872b8
David Dulau <daviddulau@gmail.com>
2018-03-20 09:55:53 +0200

    insertionSort
    added to forLicense

--- Year 3/Second Semester/For License/Nimic/Nimic/Program.cs
+++ Year 3/Second Semester/For License/Nimic/Nimic/Program.cs
@@ -6,18 +6,103 @@ using System.Threading.Tasks;
 
 namespace Nimic
 {
-    class Program
-    {
-        public static int n = 8;
-        public static int[] v = new int[n];
-        public static Random r = new Random();
-        static void Main(string[] args)
-        {
-			xxx();
+	class Program
+	{
+		public static int n = 8;
+		public static int k;
+		public static int p;
+		public static Random r = new Random();
+		public static int[] v = new int[n];
+		public static int[] sol;
+		public static bool[] fol;
+		static void Main(string[] args)
+		{
+			//xxx();
 			//selectionSort();
 			//permutari();
-            Console.ReadKey();
-        }
+			//combinari();
+			insertionSort();
+			Console.ReadKey();
+		}
+
+		private static void insertionSort()
+		{
+			GenerareV();
+			afisareV();
+
+			for (int i = 2; i < n; i++)
+			{
+				int t = v[i];
+				int j = i - 1;
+				bool ok;
+				do
+				{
+					ok = false;
+					v[j] = v[j - 1];
+					j++;
+					if ((j <= 0) || (v[j] > t))
+						ok = true;
+					Console.Write("*");
+				}
+				while (ok);
+				//todo
+				//v[j] = t;
+				afisareV();
+			}
+		}
+		
+		private static void combinari()
+		{
+			n = Convert.ToInt32(Console.ReadLine());
+			
+			p = Convert.ToInt32(Console.ReadLine());
+			fol = new bool[n];
+			sol = new int[n];
+			for (int i = 0; i < fol.Length; i++)
+			{
+				fol[i] = false;
+			}
+			GenerareSol();
+			combinari(1, n, sol, fol, p);
+			afisareSol();
+		}
+
+		private static void combinari(int k, int n, int[] sol, bool[] fol, int p)
+		{
+			if (k > p /*|| ok (sol)*/)
+			{
+				for (int i = 1; i <= p; i++)
+				{
+					Console.Write(sol[i] + " ");
+				}
+				Console.WriteLine();
+			}
+			else
+			{
+				for (int i = sol[k - 1]; i < n; i++)
+				{
+					if (fol[i] == false)
+					{
+						sol[k] = i;
+						fol[i] = true;
+						combinari(k + 1, n, sol, fol, p);
+						fol[i] = false;
+					}
+				}
+			}
+		}
+
+		//private static bool ok (int[] a)
+		//{
+		//	for (int i = 0; i < a.Length; i++)
+		//	{
+		//		if (a[i] > a[i + 1])
+		//		{
+		//			return false;
+		//		}
+		//	}
+		//	return true;
+		//}
 
 		private static void permutari()
 		{
@@ -32,7 +117,7 @@ namespace Nimic
 			{
 				c++;
 				x = true;
-				generare();
+				GenerareV();
 				for (int i = 0; i < n; i++)
 				{
 					if (v[0] != v[i])
@@ -41,23 +126,33 @@ namespace Nimic
 					}
 				}
 			}
-			afisare();
+			afisareV();
 			Console.WriteLine(c);
 		}
 
 		private static void selectionSort()
 		{
-			generare();
-			afisare();
+			GenerareV();
+			afisareV();
 			sortareSelection();
-			afisare();
+			afisareV();
 		}
 
-		private static void afisare()
+		private static void afisareV()
 		{
-			for (int i = 0; i < n; i++)
+			for (int i = 0; i < v.Length; i++)
 			{
 				Console.Write(v[i] + " ");
+			}
+			Console.WriteLine();
+			Console.WriteLine();
+		}
+
+		private static void afisareSol()
+		{
+			for (int i = 0; i < sol.Length; i++)
+			{
+				Console.Write(sol[i] + " ");
 			}
 			Console.WriteLine();
 			Console.WriteLine();
@@ -83,11 +178,19 @@ namespace Nimic
 			}
 		}
 
-		private static void generare()
+		private static void GenerareV()
 		{
-			for (int i = 0; i < n; i++)
+			for (int i = 0; i < v.Length; i++)
 			{
 				v[i] = r.Next(20);
+			}
+		}
+
+		private static void GenerareSol()
+		{
+			for (int i = 0; i < sol.Length; i++)
+			{
+				sol[i] = r.Next(20);
 			}
 		}
 	}
